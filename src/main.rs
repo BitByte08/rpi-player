7e7fef846a6d1f03a67eac26be95e167f97b5b12
bitbyte08 <me@bitworkspace.kr>
2025-12-03 19:34:57 +0900

    refactor :: Init -> YtDlp 객체로 변경

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,5 @@
-mod ui;
-use ui::{Router, Route};
-
 mod yt_dlp;
-use yt_dlp::{init};
-
-use fltk::app;
-use fltk::prelude::*;
-use fltk::window::DoubleWindow;
-use std::rc::Rc;
-use std::cell::RefCell;
-use std::time::Duration;
-
+use yt_dlp::YtDlp;
 fn main() {
-    let app = app::App::default();
-    let mut wind = DoubleWindow::new(100, 100, 500, 300, "yt-dlp GUI");
-
-    // Router 생성
-    let router = Rc::new(RefCell::new(ui::Router::new()));
-
-    wind.end();
-    wind.show();
-
-    // 처음에는 Splash 화면
-    router.borrow_mut().navigate(Route::Splash);
-    match init::Init::ensure() {
-        Ok(_) => println!("Initialization complete."),
-        Err(e) => eprintln!("Error during init: {}", e),
-    }
-    // 2초 후 Home 화면으로 전환
-    {
-        let router_clone = router.clone();
-        app::add_timeout3(2.0, move |_| {
-            router_clone.borrow_mut().navigate(Route::Home);
-        });
-    }
-
-    app.run().unwrap();
+    let _yt_dlp = YtDlp::new();
 }
--- src/yt_dlp/mod.rs
+++ src/yt_dlp/mod.rs
@@ -1,2 +1,13 @@
-pub mod init;
-pub use init::*;
+mod init;
+
+
+pub struct YtDlp {
+    pub path: std::path::PathBuf,
+}
+
+impl YtDlp {
+    pub fn new() -> Result<Self, String> {
+        let path = crate::yt_dlp::init::ensure()?;
+        Ok(Self { path })
+    }
+}
