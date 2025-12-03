a926ce7947b2e2351c9bec6193780252d75bc643
bitbyte08 <me@bitworkspace.kr>
2025-12-03 19:58:15 +0900

    del :: fltk 제거

--- src/ui/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod screens;
-pub mod router;
-
-pub use router::*;
-pub use screens::*;
--- src/ui/screens/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod home;
-pub use home::HomeScreen;
-
-pub mod splash;
-pub use splash::SplashScreen;
