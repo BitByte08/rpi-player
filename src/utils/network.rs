bd78c54737d3cc02afcef98e02d2f1e65689e8a5
bitbyte08 <me@bitworkspace.kr>
2025-12-05 09:13:13 +0900

    fix :: 운영체제별 분기 생성

--- src/utils/network.rs
+++ src/utils/network.rs
@@ -1,9 +1,15 @@
 use std::process::Command;
 
+
 pub fn internet_available() -> bool {
-    // 1회만 ping 시도
+    let args = if cfg!(target_os = "windows") {
+        vec!["-n", "1", "8.8.8.8"]
+    } else {
+        vec!["-c", "1", "8.8.8.8"]
+    };
+
     Command::new("ping")
-        .args(&["-c", "1", "8.8.8.8"]) // 구글 DNS
+        .args(&args)
         .output()
         .map(|o| o.status.success())
         .unwrap_or(false)
