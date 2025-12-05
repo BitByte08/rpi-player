a1cf570c0349912dcfdaf66486cb294ba013a20a
bitbyte08 <me@bitworkspace.kr>
2025-12-05 09:14:24 +0900

    feat :: main.rs에 테스트 코드 추가

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod yt_dlp;
+mod utils;
+mod model;
+
 use yt_dlp::YtDlp;
-fn main() {
-    let _yt_dlp = YtDlp::new();
+fn main() -> Result<(), String> {
+    let yt = YtDlp::new()?;
+    println!("[DEBUG] search 호출 전");
+    let results = yt.search("marigold")?;
+    println!("[DEBUG] search 호출 후, 결과 수: {}", results.len());
+    for r in results {
+        println!("{:?}", r);
+    }
+    Ok(())
 }
